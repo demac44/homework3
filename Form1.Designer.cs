a2ce5c3cace1b486b3ae124919419f2bafc6ddbf
Demir Umejr <u.demir@rehype.io>
2024-01-10 18:54:41 +0300

    initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Homework3
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.resultText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(407, 439);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(72, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(950, 102);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(392, 292);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 28);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.GuessNumber);
+            // 
+            // resultText
+            // 
+            this.resultText.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.resultText.Location = new System.Drawing.Point(392, 363);
+            this.resultText.Name = "resultText";
+            this.resultText.Size = new System.Drawing.Size(294, 43);
+            this.resultText.TabIndex = 3;
+            this.resultText.Text = "label2";
+            this.resultText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.resultText.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(539, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1100, 619);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultText);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Menu;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form";
+            this.Text = "Guess the number game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TextChanged += new System.EventHandler(this.GuessNumber);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label resultText;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework3
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int difference = 0;
+        public Form()
+        {
+            InitializeComponent();
+            startNewGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startNewGame() {
+            label1.Text = "I have a number between 1 and 1000. Can you guess my number? Enter your guess: ";
+            resultText.Text = "Guess result appears here.";
+            number = randomNumber.Next(1, 1000);
+            textBox1.Enabled = true;
+            textBox1.Text = "";
+            difference = -1;
+            this.BackColor = SystemColors.GrayText;
+            label2.Text = number.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GuessNumber(object sender, EventArgs e)
+        {
+            if (difference == -1)
+            {
+
+            }
+            else
+            {
+                try
+                {
+
+                    int n = Convert.ToInt32(((TextBox)sender).Text);
+
+                    if (n == number)
+                    {
+                        resultText.Text = "Correct!";
+                        this.BackColor = SystemColors.GrayText;
+                        textBox1.Enabled = false;
+
+                    }
+                    else if (number > n)
+                    {
+                        resultText.Text = "Too low.Try a higher number!";
+                        if (number - n > difference)
+                        {
+                            this.BackColor = Color.IndianRed;
+                        }
+                        else
+                        {
+                            this.BackColor = SystemColors.MenuHighlight;
+                        }
+                    }
+                    else if (number < n)
+                    {
+                        resultText.Text = "Too high.Try a lower number!";
+                        if (n - number > difference)
+                        {
+                            this.BackColor = Color.IndianRed;
+                        }
+                        else
+                        {
+                            this.BackColor = SystemColors.MenuHighlight;
+                        }
+                    }
+                }
+                catch { }
+            }
+            try
+            {
+                int nn = Convert.ToInt32(((TextBox)sender).Text);
+                if (number > nn)
+                    difference = number - nn;
+                else difference = nn - number;
+                label2.Text = difference.ToString();
+            }
+            catch { }
+        }
+    }
+}
